4033ac562645a32bc33e8d9d2cd929a35e892720
jjanggu <ssc6839@gmail.com>
2025-07-30 23:31:28 +0900

    feat: 주문 상품 리스트 조회

--- apps/commerce-api/src/main/java/com/loopers/domain/product/ProductCommand.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/product/ProductCommand.java
@@ -1,10 +1,10 @@
 package com.loopers.domain.product;
 
+import java.util.List;
+
 public record ProductCommand() {
 
-	public record GetProduct(
-		Long id
-	) {
+	public record GetProduct(Long id) {}
 
-	}
+	public record GetProducts(List<Long> ids) {}
 }
