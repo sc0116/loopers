db099407f3edbfb6b7d08d463a1aca02c171e580
jjanggu <ssc6839@gmail.com>
2025-08-01 00:25:18 +0900

    feat: 여러 브랜드 조회 기능 구현

--- apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/BrandRepository.java
@@ -1,5 +1,6 @@
 package com.loopers.domain.brand;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BrandRepository {
@@ -7,4 +8,6 @@ public interface BrandRepository {
 	Brand save(Brand brand);
 
 	Optional<Brand> findById(Long id);
+
+	List<Brand> findAllBy(List<Long> ids);
 }
--- apps/commerce-api/src/main/java/com/loopers/infrastructure/brand/BrandRepositoryImpl.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/brand/BrandRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.loopers.infrastructure.brand;
 
 import com.loopers.domain.brand.Brand;
 import com.loopers.domain.brand.BrandRepository;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -20,5 +21,10 @@ public class BrandRepositoryImpl implements BrandRepository {
 	@Override
 	public Optional<Brand> findById(final Long id) {
 		return brandJpaRepository.findById(id);
+	}
+
+	@Override
+	public List<Brand> findAllBy(final List<Long> ids) {
+		return brandJpaRepository.findAllByIdIn(ids);
 	}
 }
